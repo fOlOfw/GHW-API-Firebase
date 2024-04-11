1c31ab64e6a40ea9acab8f727c7b77f0e351550f
Filiz/feliziosso <fOlOfw@users.noreply.github.com>
2024-04-11 20:13:24 +0000

    update on server

---
+++ firebase.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+# cred = credentials.Certificate("authkey.json") 
+# firebase_admin.initialize_app(cred)
+
+#create a new document in firestore
+def create_document(collection: str, document_data: dict):
+    try:
+        db = firestore.client()
+        doc_ref = db.collection(collection).document()
+        doc_ref.set(document_data)
+        print('Document created with ID:', doc_ref.id)
+    except Exception as e:
+        print('An error occurred:', e)
+
+    #usage example
+    create_document('users', {'name': 'John Doe', 'email': 'johndoe@example.com'})
+
